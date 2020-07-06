3d3bc1f6d159a8f7114b15ebc2b014dc49ac2507
scharczynski <scharcz@brandeis.edu>
2020-07-05 23:01:36 -0400

    added bothposstim

--- models.py
+++ models.py
@@ -701,7 +701,7 @@ class GaussianBothPos(Model):
         '''One thing to try is to maybe pull out self.t as a kwarg in optimize, might allow jacobian to be calculated easier
         '''
 
-        sigma1, mu1, sigma2, mu2, a_1, a_0 = x
+        sigma1, mu1, sigma2, mu2, a_1,a_2, a_0 = x
 
         fun = (
             (a_1 * np.exp(-np.power(self.t - mu1, 2.) / (2 * np.power(sigma1, 2.))))  +
@@ -792,6 +792,92 @@ class GaussianStimBoth(Model):
 
         # return self.model(x)
         return (np.sum(fun, axis=1)/fun.shape[1])
+
+
+class GaussianStimBothPos(Model):
+
+    def __init__(self, data):
+        super().__init__(data)
+        self.param_names = ["sigma1", 
+            "mu1", 
+            "sigma2", 
+            "mu2", 
+            "a_1", 
+            "a_2",
+            "a_3", 
+            "a_4",
+            "a_5",
+            "a_6",
+            "a_7",
+            "a_8",
+            "a_0"]
+        self.t = np.tile(self.t, (self.num_trials, 1))
+
+    def info_callback(self):
+        self.stims = self.info["stim_identity"]
+        stim_matrix_1 = np.zeros((self.spikes.shape[0], 4))
+        stim_matrix_2 = np.zeros((self.spikes.shape[0], 4))
+        if self.even_odd_trials == "even":
+            trials  = list(self.stims.keys())[::2]
+        elif self.even_odd_trials == "odd":
+            trials = list(self.stims.keys())[1::2]
+        else:
+            trials  =  list(self.stims.keys())
+        #rossi-pool 1 indexed trials
+        trial_indices = [x-1 for x in list(map(int, trials))]
+
+        self.t = self.t[trial_indices]
+        for trial_num, trial in enumerate(trials):
+            stim_class = self.stims[(trial)]["sample"] 
+            if stim_class[0] == 1:
+                stim_matrix_1[int(trial_num)][:] = [1, 0, 0, 0]
+            elif stim_class[0] == 2:
+                stim_matrix_1[int(trial_num)][:] = [0, 1, 0, 0]
+            elif stim_class[0] == 3:
+                stim_matrix_1[int(trial_num)][:] = [0, 0, 1, 0]
+            elif stim_class[0] == 4:
+                stim_matrix_1[int(trial_num)][:] = [0, 0, 0, 1]
+            if stim_class[1] == 1:
+                stim_matrix_2[int(trial_num)][:] = [1, 0, 0, 0]
+            elif stim_class[1] == 2:
+                stim_matrix_2[int(trial_num)][:] = [0, 1, 0, 0]
+            elif stim_class[1] == 3:
+                stim_matrix_2[int(trial_num)][:] = [0, 0, 1, 0]
+            elif stim_class[1] == 4:
+                stim_matrix_2[int(trial_num)][:] = [0, 0, 0, 1]
+        self.stim_matrix_1 = stim_matrix_1
+        self.stim_matrix_2 = stim_matrix_2
+        return self.stim_matrix_1, self.stim_matrix_2
+
+    def model(self, x, plot=False):
+    
+        sigma1, mu1, sigma2, mu2, a_1,a_2,a_3, a_4,a_5, a_6, a_7, a_8, a_0 = x
+        fun_p1 = np.exp(-np.power(self.t - mu1, 2.) / (2 * np.power(sigma1, 2.)))
+        fun_p2 = np.exp(-np.power(self.t - mu2, 2.) / (2 * np.power(sigma2, 2.)))
+            
+        fun = (
+            (a_1*self.stim_matrix_1[:, 0] * fun_p1.T + a_5*self.stim_matrix_2[:, 0] * fun_p2.T)
+            + (a_2*self.stim_matrix_1[:, 1] * fun_p1.T + a_6*self.stim_matrix_2[:, 1] * fun_p2.T)
+            + (a_3*self.stim_matrix_1[:, 2] * fun_p1.T + a_7*self.stim_matrix_2[:, 2] * fun_p2.T)
+            + (a_4*self.stim_matrix_1[:, 3] * fun_p1.T + a_8*self.stim_matrix_2[:, 3] * fun_p2.T)
+        ) + a_0
+
+        return fun
+
+    def objective(self, x):
+        fun = self.model(x).T
+
+        obj = np.sum(self.spikes * (-np.log(fun)) +
+                      (1 - self.spikes) * (-np.log(1 - (fun))))
+
+        return obj
+
+    def plot_model(self, x):
+        fun = self.model(x)
+
+        # return self.model(x)
+        return (np.sum(fun, axis=1)/fun.shape[1])
+
 
 
 class GaussianStimBoth1(Model):
